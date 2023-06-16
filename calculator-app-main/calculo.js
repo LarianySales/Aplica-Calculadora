05d81d7a8a970e9de439cd4c96b245611a13a5a2
Lariany <110117446+LarianySales@users.noreply.github.com>
2023-06-16 17:42:32 -0300

    Add files via upload

---
+++ calculator-app-main/calculo.js
@@ -0,0 +1,27 @@
+function calcular(tipo, valor){
+    //  console.log(tipo, valor)
+    if(tipo === 'acao'){
+    //limpar a tela da calculadora
+    if(valor === 'C'){
+        document.getElementById('resultado').value = ''
+    
+    }
+    if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+        document.getElementById('resultado').value += valor
+    }
+    
+    if(valor === '='){
+      let valor_campo = eval(document.getElementById('resultado').value)
+    //   console.log(eval(valor_campo))
+    document.getElementById('resultado').value = valor_campo
+    }
+    
+    
+    }else if(tipo === 'valor'){
+        document.getElementById('resultado').value += valor
+    }
+    if(valor === 'RESET'){
+        document.getElementById('resultado').value = ''
+
+    }
+    }
